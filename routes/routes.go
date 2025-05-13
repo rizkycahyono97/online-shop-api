634ac81898af6c2457b87376a9dab4c322b9020b
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-05-13 10:43:35 +0700

    setup routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,23 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rizkycahyono97/online-shop-api/controller"
+	"github.com/rizkycahyono97/online-shop-api/middleware"
+)
+
+func SetupRoutes(r *gin.Engine, authController controller.AuthController) {
+	// public routes
+	public := r.Group("/api/v1")
+	{
+		public.POST("/register", authController.Register)
+		public.POST("/login", authController.Login)
+	}
+
+	// protected routes
+	protected := r.Group("/api/v1")
+	protected.Use(middleware.AuthMiddleware())
+	{
+		protected.GET("/")
+	}
+}
