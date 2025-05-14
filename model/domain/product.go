77962c879a9eb6fcf6c3bd4d6de7ba7baa00f33e
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-05-14 10:50:05 +0700

    add products struct model

---
+++ model/domain/product.go
@@ -0,0 +1,19 @@
+package domain
+
+import "time"
+
+type Product struct {
+	ID          uint      `json:"id" gorm:"primary_key;column:id;type:BIGINT AUTO_INCREMENT NOT NULL;"`
+	Name        string    `json:"name" gorm:"column:name;type:VARCHAR(255);column:name; NOT NULL;"`
+	Description string    `json:"description" gorm:"column:description;type:TEXT; NOT NULL;"`
+	Price       float64   `json:"price" gorm:"column:price;type:DECIMAL(10, 2); NOT NULL;"`
+	Stock       int       `json:"stock" gorm:"column:stock;type:INT; NOT NULL;"`
+	ImageURL    string    `json:"image_url" gorm:"column:image_url;type:VARCHAR(255);"`
+	CreatedAt   time.Time `json:"created_at" gorm:"type:DATETIME;column:created_at; autoCreateTime"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"type:DATETIME;column:updated_at; autoUpdateTime"`
+}
+
+// nama table
+func (Product) TableName() string {
+	return "products"
+}
