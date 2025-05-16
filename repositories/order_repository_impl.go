721ea03d3e4cb2dd3ac0005b489b2c9ab98a9874
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-05-16 14:45:57 +0700

    add order_repository_impl

--- repositories/order_repository_impl.go
+++ repositories/order_repository_impl.go
@@ -13,22 +13,40 @@ func NewOrderRepositoryImpl(db *gorm.DB) OrderRepository {
 	return &OrderRepositoryImpl{db: db}
 }
 
-func (o OrderRepositoryImpl) CreateOrder(order *domain.Order) (*domain.Order, error) {
-	//TODO implement me
-	panic("implement me")
+func (r OrderRepositoryImpl) CreateOrder(order *domain.Order) (*domain.Order, error) {
+	if err := r.db.Create(order).Error; err != nil {
+		return nil, err
+	}
+	return order, nil
 }
 
-func (o OrderRepositoryImpl) CreateOrderItems(items []*domain.OrderItem) error {
-	//TODO implement me
-	panic("implement me")
+func (r OrderRepositoryImpl) CreateOrderItems(items []*domain.OrderItem) error {
+	if err := r.db.Create(items).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
-func (o OrderRepositoryImpl) GetOrderByUserID(userID uint) ([]*domain.Order, error) {
-	//TODO implement me
-	panic("implement me")
+func (r OrderRepositoryImpl) GetOrderByUserID(userID uint) ([]*domain.Order, error) {
+	var orders []*domain.Order
+	err := r.db.Preload("OrderItems.Product").Preload("payment").
+		Where("user_id = ?", userID).
+		Order("created_at desc").
+		Find(&orders).Error
+	if err != nil {
+		return nil, err
+	}
+	return orders, nil
 }
 
-func (o OrderRepositoryImpl) GetAllOrders() ([]*domain.Order, error) {
-	//TODO implement me
-	panic("implement me")
+func (r OrderRepositoryImpl) GetAllOrders() ([]*domain.Order, error) {
+	var orders []*domain.Order
+	err := r.db.Preload("User").Preload("OrderItems.Product").Preload("payment").
+		Order("created_at desc").
+		Find(&orders).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return orders, nil
 }
